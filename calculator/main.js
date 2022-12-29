b15ec2f036881a7a2dec51ca7509664664e1fe72
Bartosz Zawadka <bart.zaw00@gmail.com>
2022-12-29 17:11:09 +0100

    [Calculator] calculator layout done + js started

---
+++ calculator/main.js
@@ -0,0 +1,90 @@
+const currentNumber = document.querySelector('.currentNumber');
+
+const previousNumber = document.querySelector('.previousNumber p');
+
+const mathSign = document.querySelector('.mathSign');
+
+const numbersButtons = document.querySelectorAll('.number');
+
+const operatorsButtons = document.querySelectorAll('.operator');
+
+const equalsButton = document.querySelector('.equals');
+
+const clearButton = document.querySelector('.clear');
+
+const calculatorHistory = document.querySelector('.history');
+
+const historyBtn = document.querySelector('.history-btn');
+
+
+let result = '';
+
+
+function displayNumbers() {
+    /* Jeżeli ktoś kliknie kropkę to currentNumber będzie zawierał kropkę */
+    if(this.textContent === '.' && currentNumber.innerHTML.includes('.')) 
+        return;
+    if(this.textContent === '.' && currentNumber.innerHTML === '') 
+        return currentNumber.innerHTML = '.0';
+
+    currentNumber.innerHTML += this.textContent;
+}
+
+function operate() {
+
+    if(currentNumber.innerHTML === '' && this.textContent === '-') {
+        currentNumber.innerHTML = '-';
+        return; 
+    }
+    /* nie pokazuj operatorow na ekranie, gdy jest on pusty */
+    else if(currentNumber.innerHTML === '')
+        return;
+
+    if(mathSign.innerHTML !== '')
+        showResult();
+    
+    previousNumber.innerHTML = currentNumber.innerHTML;
+    mathSign.innerHTML = this.textContent;
+    currentNumber.innerHTML = '';
+}
+
+function showResult() {
+
+}
+
+function clearScreen() {
+
+}
+
+function clearHistory() {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Nasluchiwanie przyciskow
+
+operatorsButtons.forEach((button) => button.addEventListener('click', operate));
+
+equalsButton.addEventListener('click', showResult);
+
+clearButton.addEventListener('click', clearScreen);
+
+numbersButtons.forEach((button) => {
+    button.addEventListener('click', displayNumbers);
+})
+
+historyBtn.addEventListener('click', clearHistory);
