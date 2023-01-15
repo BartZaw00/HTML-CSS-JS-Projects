8f42bbe6b1b1f9c9b9e8e21f1f2923d1f2bc06a8
Bartosz Zawadka <bart.zaw00@gmail.com>
2023-01-15 20:46:27 +0100

    [Scroll] added scroll navbar behaviour

--- scroll/app.js
+++ scroll/app.js
@@ -1,8 +1,12 @@
 const menuBtn = document.querySelector('[data-menu-button]');
 
-
+const navbar = document.querySelector('[data-navbar]');
 
 
 menuBtn.addEventListener('click', () => {
-    
+
+});
+
+window.addEventListener("scroll", function() {
+    navbar.classList.add('shrink');
 });
