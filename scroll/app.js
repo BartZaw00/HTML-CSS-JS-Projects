a5ba5f4d33f929fa7a188a18c3357f2466d79669
Bartosz Zawadka <bart.zaw00@gmail.com>
2023-01-16 14:55:09 +0100

    [Scroll] scroll + menu click operations done

--- scroll/app.js
+++ scroll/app.js
@@ -2,12 +2,15 @@ const menuBtn = document.querySelector('[data-menu-button]');
 
 const navbar = document.querySelector('[data-navbar]');
 
+const menuScroll = document.querySelector('[data-menu-scroll]');
+
 
 menuBtn.addEventListener('click', () => {
-
+    // Dodaje klasę "active", jeśli nie istnieje i usunie ją, jeśli istnieje
+    menuScroll.classList.toggle('active'); 
 });
 
-window.addEventListener("scroll", function() {
+window.addEventListener("scroll", function () {
     if (window.scrollY < 100) navbar.classList.remove('shrink');
     else navbar.classList.add('shrink');
 });
