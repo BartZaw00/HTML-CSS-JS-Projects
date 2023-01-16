e66488f6325cadfde6e5426e3377c253931eb64e
Bartosz Zawadka <bart.zaw00@gmail.com>
2023-01-16 15:16:53 +0100

    [Scroll] scroll button added in the right-bottom corner and working

--- scroll/app.js
+++ scroll/app.js
@@ -4,13 +4,17 @@ const navbar = document.querySelector('[data-navbar]');
 
 const menuScroll = document.querySelector('[data-menu-scroll]');
 
+const arrowUpButton = document.querySelector('[data-arrow-up-button]');
+
 
 menuBtn.addEventListener('click', () => {
     // Dodaje klasę "active", jeśli nie istnieje i usunie ją, jeśli istnieje
-    menuScroll.classList.toggle('active'); 
+    menuScroll.classList.toggle('active');
 });
 
 window.addEventListener("scroll", function () {
     if (window.scrollY < 100) navbar.classList.remove('shrink');
     else navbar.classList.add('shrink');
+    if (window.scrollY < 500) arrowUpButton.classList.remove('display-block');
+    else arrowUpButton.classList.add('display-block');
 });
