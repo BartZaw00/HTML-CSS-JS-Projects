a02ba9e16dc3f9bb8f065d5cfb6b51c7bc16cb15
Bartosz Zawadka <bart.zaw00@gmail.com>
2023-01-21 17:05:45 +0100

    [Color-flipper] refactor done

--- color-flipper/app.js
+++ color-flipper/app.js
@@ -9,37 +9,46 @@ const clickmeBtn = document.querySelector('[data-clickme-button]');
 const dataColor = document.querySelector('[data-color');
 
 const colorArray = ['blue', '#f1f5f8', 'rgb(120,100,1)', '#a3f2b3', '#31ef8f', 'rgb(10,105,10)', 'rgb(210,5,120)', 'yellow', '#abc321', '#ffe3a1'];
-let randomNumber = 0;
+
 let numbers = [];
 let result = '';
 let hexaChars = '';
 let state = "a";
 
-simpleBtn.addEventListener('click', () => {
-    state = "a";
+window.addEventListener('load', () => {
     simpleBtn.style.color = 'rgb(10,90,10)';
-    hexBtn.style.color = "#000";
+});
+
+simpleBtn.addEventListener('click', () => {
+    if (state === 'b')
+        state = 'a';
+    if (simpleBtn.style.color = 'rgb(0,0,0)')
+        simpleBtn.style.color = 'rgb(10,90,10)';
+    if (hexBtn.style.color = 'rgb(10,90,10')
+        hexBtn.style.color = 'rgb(0,0,0)';
 })
 
 hexBtn.addEventListener('click', () => {
-    state = "b";
-    simpleBtn.style.color = '#000';
-    hexBtn.style.color = "rgb(10,90,10)";
+    if (state === 'a')
+        state = 'b';
+    if (simpleBtn.style.color = 'rgb(10,90,10)')
+        simpleBtn.style.color = 'rgb(0,0,0)';
+    if (hexBtn.style.color = 'rgb(10,90,10')
+        hexBtn.style.color = 'rgb(10,90,10)';
 })
 
 clickmeBtn.addEventListener('click', () => {
     if (state === "a") {
-        randomNumber = getRandomNumber();
-        dataColor.innerHTML = colorArray[randomNumber];
-        dataColor.style.color = colorArray[randomNumber];
-        container.style.background = colorArray[randomNumber];
+        randomIndex = getRandomIndex();
+        dataColor.innerHTML = colorArray[randomIndex];
+        dataColor.style.color = colorArray[randomIndex];
+        container.style.background = colorArray[randomIndex];
     }
     else if (state === "b") {
         numbers = [];
         for (let i = 0; i < 6; i++)
-            numbers.push(Math.floor(Math.random() * 16));
+            numbers.push(getRandomNumber());
         hexaChars = numbers.map(num => {
-            console.log('numer:' + num)
             if (num > 9) return String.fromCharCode(num + 87);
             return num;
         });
@@ -48,8 +57,14 @@ clickmeBtn.addEventListener('click', () => {
         dataColor.style.color = result;
         container.style.background = result;
     }
+    else
+        console.log('error');
 })
 
-function getRandomNumber() {
+function getRandomIndex() {
     return Math.floor(Math.random() * 10);
+}
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * 16);
 }
