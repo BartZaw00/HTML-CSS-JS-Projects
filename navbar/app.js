be342e5b3ad47482e1ddc3b7c88cda3bf0b4f8ae
Bartosz Zawadka <bart.zaw00@gmail.com>
2023-01-21 22:05:34 +0100

    [Navbar] refactor done

--- navbar/app.js
+++ navbar/app.js
@@ -5,7 +5,7 @@ const sidebar = document.querySelector('[data-sidebar]');
 
 
 sideBtn.addEventListener('click', () => {
-    if (sidebar.style.right == '0px') {
+    if (sidebar.style.right === '0px') {
         sidebar.style.right = '-250px';
         sideBtn.style.backgroundImage = 'url("images/menu.png")';
     }
