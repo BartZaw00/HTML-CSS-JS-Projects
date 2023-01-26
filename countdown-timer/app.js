b1f50915b328926d8b80f98f284dbb6272fdb337
Bartosz Zawadka <bart.zaw00@gmail.com>
2023-01-26 13:58:44 +0100

    [Countdown-timer] project done

--- countdown-timer/app.js
+++ countdown-timer/app.js
@@ -1,34 +1,34 @@
 const countdownElement = document.querySelectorAll('.countdown');
-console.log(countdownElement)
 
 // Tworzymy obiekt Date i ustawiamy czas końcowy
-let deadline = new Date("2023-02-18 20:00");
-
-console.log(deadline)
+let deadline = new Date(2023, 3, 2, 18, 0, 0);
 
 let actualDate = new Date();
 
-let timeLeftInSeconds = deadline.getSeconds() - actualDate.getSeconds();
+let seconds = Math.floor((deadline - actualDate) / 1000);
 
-let timeLeft = new Date(timeLeftInSeconds);
+let minutes = Math.floor(seconds / 60);
 
-console.log()
+let hours = Math.floor(minutes / 60);
 
-// Ustawiamy interwał, który co sekundę aktualizuje pozostały czas
+let days = Math.floor(hours / 24);
+
+
 let interval = setInterval(() => {
     actualDate = new Date();
-    timeLeftInSeconds = deadline - actualDate;
-    timeLeft = new Date(timeLeftInSeconds);
+    seconds = Math.floor((deadline - actualDate) / 1000);
+    minutes = Math.floor(seconds / 60) % 60;
+    hours = Math.floor(seconds / 3600) % 24;
+    days = Math.floor(seconds / 3600 / 24);
 
     // Jeśli czas minął, kończymy interwał
-    if (timeLeftInSeconds <= 0) {
+    if (Math.floor((deadline - actualDate) / 1000) <= 0) {
         clearInterval(interval);
     } else {
-        console.log(`Aktualna data: ${actualDate}.`);
-        console.log(`Deadline data: ${deadline}.`);
-        countdownElement[0].firstElementChild.textContent = timeLeft.getDate();
-        countdownElement[1].firstElementChild.textContent = timeLeft.getHours();
-        countdownElement[2].firstElementChild.textContent = timeLeft.getMinutes();
-        countdownElement[3].firstElementChild.textContent = timeLeft.getSeconds();
+        countdownElement[0].firstElementChild.textContent = days;
+        countdownElement[1].firstElementChild.textContent = hours;
+        countdownElement[2].firstElementChild.textContent = minutes;
+        countdownElement[3].firstElementChild.textContent = seconds % 60;
     }
-}, 1000);
+}
+    , 1000);
